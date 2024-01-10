f55dee38779c073c0e896a77d7382d253d81b54d
panda0330 <superdev330@gmail.com>
2024-01-11 04:19:53 +0900

    add link

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=4v1jlu^ube&*sqqr+&km!wlhy629rwf4j8nf3o-(07f*dpv&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
